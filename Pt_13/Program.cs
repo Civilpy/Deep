a5fae5e09a31429172fc17cb8bda416fe9ff989a
George <amit.gh.civil98@gmail.com>
2024-08-15 23:48:37 +0330

    Streams

--- Pt_13/Program.cs
+++ Pt_13/Program.cs
@@ -51,11 +51,41 @@ Console.WriteLine();
 // that aren't in the ASCII character set? 
 
 
-string unsupportedAsciiString = "im in Danger!";
-byte[] unsupportedAsciiBytes = Encoding.ASCII.GetBytes(unsupportedAsciiString);
-string convertedBackFromUnsupportedAscii
+string unsupportedAsciiString = "😊 im in Danger!"; // in C# when we are declaring strings and they're being used this is treated as something that is not ASCII //😊 im in Danger!
+byte[] unsupportedAsciiBytes = Encoding.ASCII.GetBytes(unsupportedAsciiString); // when we done the conversion back and forth this one has ?? marks ==> ?? im ....
+string convertedBackFromUnsupportedAscii = Encoding.ASCII.GetString(unsupportedAsciiBytes);
+Console.WriteLine("Converting to ASCII and back with unsupported characters");
+Console.WriteLine($"Original: {unsupportedAsciiString}");// => ?? im ....
+Console.WriteLine($"Converted: {convertedBackFromUnsupportedAscii}");// => ?? im ....
+Console.WriteLine($"Original string length: {unsupportedAsciiString.Length}"); //17
+Console.WriteLine($"Converted string length: {convertedBackFromUnsupportedAscii.Length}");//17
+Console.WriteLine($"      ASCII Bytes length: {unsupportedAsciiString.Length}");//17
+Console.WriteLine($"String Equal: {unsupportedAsciiString == convertedBackFromUnsupportedAscii}");//false
+Console.WriteLine($"First Char Equals: {unsupportedAsciiString[0] == convertedBackFromUnsupportedAscii[0]}");//false
+Console.WriteLine();
 
 
+
+
+
+
+// to handle this, we can use "Unicode", which is a standard
+// that defines a much larger character set
+// and we'll use UTF-8, which is a way to encode Unicode characters
+// to make this example works
+
+byte[] unsupportedStringAsUtf8Bytes = Encoding.UTF8.GetBytes(unsupportedAsciiString);
+string unsupportedStringAsUtf8 = Encoding.UTF8.GetString(unsupportedStringAsUtf8Bytes);
+Console.WriteLine("Converting to UTF-8 and back with unsupported characters");
+Console.WriteLine($"Original: {unsupportedAsciiString}");
+Console.WriteLine($"Converted: {unsupportedStringAsUtf8}");
+Console.WriteLine($"Original string length: {unsupportedAsciiString.Length}"); //17
+Console.WriteLine($"Converted string length: {unsupportedStringAsUtf8.Length}");//17
+Console.WriteLine($"ASCII Bytes length: {unsupportedAsciiBytes.Length}");//17
+Console.WriteLine($"UTF8 Bytes length: {unsupportedStringAsUtf8Bytes.Length}");//19
+Console.WriteLine($"String Equal: {unsupportedAsciiString == unsupportedStringAsUtf8}");//false
+Console.WriteLine($"First Char Equals: {unsupportedAsciiString[0] == unsupportedStringAsUtf8[0]}");//false
+Console.WriteLine();
 
 
 
---
+++ Pt_14/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Streams in C# allow  us to work with data
+ * in a more abstract way than just having 
+ * a byte array.
+ * (so we sont have think about the implementation 
+ * of whats backing from the Stream. so it does not matter to 
+ * us if its a byte array or if its something coming from the network
+ * if its a file ... it could be anything ..it doesnt matter if the consumer
+ * of the Stream API, all we know is we have A stream API to work with
+ * 
+ * Stream allows us to navigate through a set of bytes and depending on the 
+ * type of the Stream certain operation are not allowed 
+ * )
+ * Streams provide us a common API for working
+ * with binary data without having to know 
+ * exactly how that data is represented
+ * behind the scenes!
+ * 
+ * 
+ * Remember inheritance ? there is a base class
+ * for all streams called Stream, which is abstract.
+ * Stream stream = new Stream(); // won't cpmpile!
+ *  
+ * 
+ * Streams provide  information about the data source 
+ * that wee're working with, like:
+ * - the length of the data
+ * - the current position in the data
+ * - wheter we can read from or write to the data
+ * 
+ * Its important to note that all Streams get 
+ * these Properties/methods bcuz they inherit
+ * from the base... but not all of them support
+ * all of the fuctionality!
+ * 
+ * 
+ * let's start with one of the most basic streams,
+ * the MemoryStream
+ * 
+ */
+
+
+using System.Text;
+
+MemoryStream ms = new MemoryStream();
+
+// we can write a data to a memory stream
+
+Console.WriteLine("writing data to memory stream...");
+Console.WriteLine($"Stream Position Before: {ms.Position}"); // position means where we pointing at with end of set of bytes that
+                                                             // Stream is representing starting at zero at the very begining
+                                                             // and whatever position that we happen to be at after reading 
+                                                             // so as we read through Stream , that position changes  
+                                                             // if we have 5 bytes  of an array that would mean the length of the memory Stream 
+                                                             // is going to be 5
+
+
+Console.WriteLine($"Stream Length Before: {ms.Length}");     // Capacity is going to be something on the memory string that tells
+                                                             // us how much Capacity in the Array that we are using exist 
+
+Console.WriteLine($"Stream Capacity Before: {ms.Capacity}");// its different from length its bcuz theres an optimization of a place that 
+                                                            // we dont want to have the capacity alwaya excatly Equal to length ,
+                                                            // bcuz if we need to change the size of the Array behind the secne to get more data , the capacity of that Array can change thats up to the implmentation of the memory Stream to manage for us 
+
+
+byte[] data = Encoding.UTF8.GetBytes("Hello, world, From CivilPy");
+ms.Write(
+    data,
+    0,
+    data.Length);
+
+Console.WriteLine($"Stream Position Before: {ms.Position}");
+Console.WriteLine($"Stream Length Before: {ms.Length}");
+Console.WriteLine($"Stream Capacity Before: {ms.Capacity}");
+Console.WriteLine();
+

350b92b10f75ae8ad3eebd424340ee962d7cd6ca
George <amit.gh.civil98@gmail.com>
2024-08-16 15:38:08 +0330

    Files

--- Pt_14/Program.cs
+++ Pt_14/Program.cs
@@ -65,12 +65,101 @@ Console.WriteLine($"Stream Capacity Before: {ms.Capacity}");// its different fro
 
 byte[] data = Encoding.UTF8.GetBytes("Hello, world, From CivilPy");
 ms.Write(
-    data,
-    0,
-    data.Length);
+    data, // a set of bytes 
+    0, // offset => where to start 
+    data.Length); // how many to go 
 
 Console.WriteLine($"Stream Position Before: {ms.Position}");
 Console.WriteLine($"Stream Length Before: {ms.Length}");
 Console.WriteLine($"Stream Capacity Before: {ms.Capacity}");
 Console.WriteLine();
+
+
+
+/*
+ * if we wanted to read the data back out of the stream,
+ * it's important to note the position we're currently in now
+ * 
+ * let's get back to the start using "Seek", which allows
+ * us to specify where we're seeking relative to
+ * 
+ */
+
+
+/*
+ * what we want to do is start at the original position and 
+ * that's bcuz az we wrote data in that memory stream 
+ * like we saw where we pointing to is at the end of the data that we wrote 
+ * but if we want to read that data back we need to be able to 
+ * move that position all the way to the begining so theres a couple of diffrenet ways 
+ * to do that ... memory stream has a Method calls   Seek   
+ */
+Console.WriteLine("Repositioning memory stream...");
+ms.Seek(0, SeekOrigin.Begin); // as we providing here 0 for the offset and
+                              // seek from the beginign of the stream go over zero 
+                              // when this code runs we go back to the begining of the stream 
+                              // and its the same Stream that we wrote data into  
+
+//or
+
+ms.Position = 0;
+Console.WriteLine($"Stream position After : {ms.Position}");
+
+
+/*
+ * now we can read out data back out!
+ * ... but how much data do we need to read?
+ * .. where are we putting it?
+ * 
+ */
+
+/*
+ * so if we want to go read some stuff out of here..how do we know 
+ * how many data do we have to read and when we go to read that data 
+ * like, where is it going , whats the result of getting that data out
+ * do have to give it a container to go put that data into  =====> the answer is the second one , we need to be able to
+ *                                                                 create a byte array , so another Buffer that we can go 
+ *                                                                 read that data into 
+ *                                                                 
+ *                                                                 
+ *                                                                 imagine like this: we have a stream and that's going to be 
+ *                                                                 pointing at some data that we just wrote to the Stream , 
+ *                                                                 it has its own Arraay, now when we're going to read data back out
+ *                                                                 we need to be able to say, where did that data go 
+ *                                                                 so we essentioally making a copy of some data back out of the stream and 
+ *                                                                 bcuz we're copying data back out we need somewhere to put it we
+ *                                                                 dont have our own spot to go keep that data yet 
+ *                                                                 
+ */
+
+
+
+byte[] readBuffer = new byte[ms.Length];
+Console.WriteLine("Reading data from memory stream ...");
+int numberOfBytesRead = ms.Read(
+    readBuffer, // here we give it a buffer ... this buffer can be
+                // bigger than what we want to read out but it should
+                // not be smaller or else we're going to have an issue 
+                // so if we want to read 1000 bytes of something 
+                // and u only had a 10 byte buffer that's not gonna work how u might except 
+    0, // position 
+    readBuffer.Length ); // length
+
+Console.WriteLine($"Number of bytes read: {numberOfBytesRead}");
+
+string readString = Encoding.UTF8.GetString( readBuffer );
+Console.WriteLine($"Read string: {readString}");
+Console.WriteLine();
+
+
+// also important to note that MemoryStream
+// has a little "shortcut" for getting the bytes:
+
+Console.WriteLine("Reading data from memory Stream using ToArray()...");
+byte[] memoryStreamBytes =  ms.ToArray();
+readString = Encoding.UTF8.GetString( memoryStreamBytes );
+Console.WriteLine($"Read String: {readString}");
+
+
+
 
---
+++ Pt_15/Program.cs
@@ -0,0 +1,25 @@
+﻿// we can read and write files using built-in classes in C#
+
+// there are many convenience methods that we have access to :
+
+using System.Text;
+
+File.WriteAllText(
+    "some-file.txt",
+    "hello, wolrd!");
+File.WriteAllBytes(
+    "some-file.txt",
+    Encoding.UTF8.GetBytes("Hello, wolrd from Amir bbb"));
+byte[] someFileBytes = File.ReadAllBytes("some-file.txt");
+string someFileString  = File.ReadAllText("some-file.txt");
+
+// we can use similar methods to gain access to a stream !
+
+FileStream fileStream = File.Open(
+    "some-file.txt",
+    FileMode.OpenOrCreate,
+    FileAccess.Write,
+    FileShare.Read);
+
+byte[] buffer = Encoding.UTF8.GetBytes("Hello, world!");
+fileStream.Write(buffer, 0, buffer.Length);

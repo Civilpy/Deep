c2e0859580a87eb40fd75c1c9048c143088e8ecd
George <amit.gh.civil98@gmail.com>
2024-08-18 15:55:28 +0330

    Linq _ Extension Method

---
+++ Pt_16/Program.cs
@@ -0,0 +1,71 @@
+﻿/* 
+ * Extension methods in C# allow us to add new methods
+ * to existing types without modifying the original type
+ * or creating a new derived type
+ * 
+ * 
+ * 
+ * The Requirement are:
+ * - we need a static class
+ * - we need a static method on the class
+ * - we need the "this" keyword on the parameter that we are "extending"
+ * - the Parameter marked with "this" must be the first parameter
+ * 
+ * 
+ * Extended method in C# Are a special type of static method on a static class
+ * so their requirement we need to be able for this to work are 
+ * 
+ * 
+ */
+
+
+// so the name we gave to that class , it doesnt really matter as
+// long as we are not trying to call it this way :
+var reversedStr = Extensions.Reverse("Hello, world");
+// we can infact call it this way but like a static method on any static class 
+// but if we are using it like an extension method 
+
+
+// but when we call it like an extension method 
+// we get this really cool syntax where it looks
+// like a Reverse() is a method thats built
+// into the string class!
+var forwardStr = reversedStr.Reverse();
+
+// this is where it looks really powerful for us
+// bcuz it looks like we now added this Reverse Method directly on to the string
+// this is the same method tho like above 
+
+
+// there's a popular part of dotnet that uses extension methods
+// called LINQ 
+
+IEnumerable<int> numbers = new List<int> { 1,2,3,4,5 };
+
+// nearly all of the methods we see in intellisense
+// when accessing numbers are extension methods 
+// from LINQ
+var evenNUmbers =  numbers.Where(n => n % 2 == 0);
+
+/*
+ * The requirements are :
+ *  - WeakReference need a static class 
+ *  - we need a static method on the class
+ *  - we need the "this" keyword  on the parameter that we are "extending"
+ *  - The Parameter marked with "this" must be the first Parameter
+ */
+ // these are helpful for creating some helper fuction and you wanna be able 
+ // to make the code read really nicely 
+
+
+
+public static class Extensions
+{
+    public static string Reverse(
+        this string str)
+    {
+        var reversedChars = str.Reverse<char>().ToArray();
+        var reversed = new string(reversedChars);
+        return reversed;
+    }
+}
---
+++ Pt_17/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * LINQ stands for language INtegrated Query
+ * we get access to a bunch of LINQ methods in the System.Linq namespace
+ *that operate on IEnumerable<T> => this means they have all of these static classes that take in this
+ *                                  Ienumerable and we are able to write a method that essentioally looks 
+ *                                  like its Adding new Functionality to Ienumerable 
+ *They're all... extension methods!
+ *
+ *  LINQ Extension Method does bonch of different things for us 
+ *  but Generally map these 3 Categories 
+ *
+ *LINQ can help us
+ * - map : transform each item => transforming things as we looking through a set of items 
+ *                                so going through each item in the IEnumerable and doing something with it 
+ *                                
+ * - filter: only take some items = which is going to be reducing what we are looking at when it comes to 
+ *                                  operating on IEnumerbale 
+ *                                  
+ * - reduce: combine items => which is going to be going from IEnumerable to a calculated value at the End 
+ * 
+ * map: transform each element in a collection
+ * 
+ */
+
+List<string> rawNumbers = ["1", "2", "3" , "4",  "5"]; // list implment IEnumerable 
+List<int> numbers = new();
+foreach(string rawNumber in rawNumbers)
+{
+    numbers.Add(int.Parse(rawNumber));
+}
+// The basi LINQ method for mapping is select:
+ var number2 = rawNumbers.Select(number => int.Parse(number)).ToList();
+
+
+
+
+// FIlter : remove elements from a colletion
+List<int> evenNUmbers = new();
+foreach(int number in numbers)
+{
+    if(number % 2 == 0)
+    {
+        evenNUmbers.Add(number);
+    }
+}
+
+ // using LINQ we could do ...
+
+var evenNumber2 = evenNUmbers.Where(x => x % 2 == 0).ToList(); 
